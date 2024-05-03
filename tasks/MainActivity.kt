25e68db76690abce70a2457f97271bc6e067d640
Julia <124000173+jubck@users.noreply.github.com>
2024-05-02 21:30:24 -0400

    submission

---
+++ tasks/MainActivity.kt
@@ -0,0 +1,16 @@
+/*
+ @author Charlize Aponte
+ Date: 04/3/24
+ Assignment: Chapter 14 demo
+ */
+package com.hfad.tasks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
